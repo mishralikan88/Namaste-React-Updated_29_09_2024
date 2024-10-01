a04e660f5c228e03fcb553ea6ad48d73f9737e7c
mishralikan88 <mishralikan88@gmail.com>
2024-10-01 12:28:34 +0530

    Ignited my app with parcel

--- NamasteReact_Igniting_Our_App/app.js
+++ NamasteReact_Igniting_Our_App/app.js
@@ -1,16 +1,18 @@
-// creating h1 react element
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", {
-    id: "heading",
-    xyz: "abc"
-}, "Hello world from React")
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h1", {}, "I am an h2 tag")
 
-// creating a root
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h1", {}, "I am an h2 tag")
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+    ]),
+]);
 
-// appending the h1 react element inside the root using render()
-
-root.render(heading)
-
-console.log(heading) // heading is a react element
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
